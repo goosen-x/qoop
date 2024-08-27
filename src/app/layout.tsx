6fd49b50f3ad2001f8de7189363e26e0872e1647
Dmitry Borisenko <brdima92@gmail.com>
2024-08-27 11:35:43 +0300

    feat/ add header & images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,7 +15,23 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <header className="bg-zinc-600 p-6 text-white">
+          <nav>
+            <ul className="flex gap-4">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+            </ul>
+          </nav>
+
+          <Link href="/">Sign in</Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+
+const links = [
+  "https://utfs.io/f/abe98bdd-043f-4d05-8564-c3bf7e16da65-1d.webp",
+  "https://utfs.io/f/a88c1ef9-fda2-448c-8b64-d70c6ab02c02-1e.webp",
+  "https://utfs.io/f/0e25d45f-185c-4b7a-99fd-783bc970ff1e-1f.webp",
+  "https://utfs.io/f/f1878c37-eef4-4583-88fe-dc29d65161f3-1g.webp",
+  "https://utfs.io/f/6e4f9b9a-3b12-4162-b859-34f42e02d542-1h.webp",
+  "https://utfs.io/f/7257b023-9ae1-49da-9eca-c4ed553dd9d0-1i.webp",
+];
+
 export default function HomePage() {
+  const mockImages = links.map((url, idx) => ({ id: idx + 1, url }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h2>hello</h2>
+    <main className="p-4">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <Image
+            src={image.url}
+            alt="random"
+            width={200}
+            height={200}
+            key={image.id}
+          />
+        ))}
+      </div>
     </main>
   );
 }
