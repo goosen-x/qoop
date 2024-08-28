4c3bcf515d8a7ac41534d7c0fc76b2fb03373093
Dmitry Borisenko <brdima92@gmail.com>
2024-08-28 10:57:52 +0300

    replace server logic

--- src/components/sections/gallery.tsx
+++ src/components/sections/gallery.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const SectionGallery = async () => {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
       {images.map((image, idx) => (
         <Image
+          className="object-cover"
+          // objectFit="fill"
           src={image.url}
           alt="random"
           width={200}
